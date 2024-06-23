ad190c87cf8385b0699da11287ec85e205625d66
Nisarg Lo <140806157+NisargLo@users.noreply.github.com>
2024-06-23 13:13:03 +0530

    Update Lab_01.java

--- Lab 01, 02/Lab_01.java
+++ Lab 01, 02/Lab_01.java
@@ -5,8 +5,8 @@ import java.util.*;
 public class Lab_01 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        double r=sc.nextDouble();
         System.out.println("Enter radius :");
+        double r=sc.nextDouble();
         System.out.println("Area = "+(Math.PI*Math.pow(r, 2)));
     }
 }
