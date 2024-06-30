f97d279697c6de362376c329ed8f8715e5a3df44
Nisarg Lo <140806157+NisargLo@users.noreply.github.com>
2024-06-30 11:27:10 +0530

    Create Cryptography.java

---
+++ Extra DS Programs/Cryptography.java
@@ -0,0 +1,59 @@
+// Caesar Cipher Method
+
+import java.util.Scanner;
+
+public class Cryptography {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Text :");
+        String plaintext=sc.nextLine();
+        System.out.println("Enter Caesar Cipher (K) :");
+        int K=sc.nextInt();
+        Caesar_Cipher_Method ccm=new Caesar_Cipher_Method(plaintext, K);
+        System.out.println("Encrypt or Decrypt?");
+        String choose=sc.next().trim();
+        if(choose.equalsIgnoreCase("Encrypt")){
+            System.out.println("Ciphertext - "+ccm.encryption());
+        }
+        else if(choose.equalsIgnoreCase("Decrypt")){
+            System.out.println("Plaintext - "+ccm.decryption());
+        }
+        else{
+            System.out.println("Invalid");
+        }
+    }
+}
+
+class Caesar_Cipher_Method{
+    private final char[] charArry;
+    private final byte K;
+
+    private byte calculateK(int K){
+        K %= 26;
+        return (byte)K;
+    }
+
+    Caesar_Cipher_Method(String text, int K){
+        this.charArry=text.toCharArray();
+        if(K>26 || K<-26){
+            this.K=calculateK(K);
+        }
+        else{
+            this.K=(byte)K;
+        }
+    }
+
+    protected String encryption(){
+        for (int i=0;i<this.charArry.length;i++) {
+            this.charArry[i]=(char)(this.charArry[i]+K);
+        }
+        return new String(this.charArry);
+    }
+
+    protected String decryption(){
+        for (int i=0;i<this.charArry.length;i++) {
+            this.charArry[i]=(char)(this.charArry[i]-K);
+        }
+        return new String(this.charArry);
+    }
+}
