92a61a3b6561b03c7de8f857f4a811d3cdd1c1bd
Nisarg Lo <140806157+NisargLo@users.noreply.github.com>
2024-07-04 20:17:39 +0530

    Update Lab_57.java

--- Lab 10/Lab_57.java
+++ Lab 10/Lab_57.java
@@ -1,11 +1,11 @@
 /*
-    57. Write a menu driven program to implement following operations on the singly linked list.
-     Insert a node at the front of the linked list.
-     Display all nodes.
-     Delete a first node of the linked list.
-     Insert a node at the end of the linked list.
-     Delete a last node of the linked list.
-     Delete a node from specified position.
+    Write a menu driven program to implement following operations on the singly linked list.
+        Insert a node at the front of the linked list.
+        Display all nodes.
+        Delete a first node of the linked list.
+        Insert a node at the end of the linked list.
+        Delete a last node of the linked list.
+        Delete a node from specified position.
  */
 
 public class Lab_57 {
