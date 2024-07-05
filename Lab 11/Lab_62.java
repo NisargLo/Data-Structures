c12e6bab4860ca51e3cf8f920e217c722f883457
Nisarg Lo <140806157+NisargLo@users.noreply.github.com>
2024-07-05 23:18:09 +0530

    Create Lab_62.java

---
+++ Lab 11/Lab_62.java
@@ -0,0 +1,45 @@
+/*
+    WAP to remove duplicate elements from a singly linked list.
+
+    class Linked_List is used from Lab_57
+ */
+
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Lab_62 {
+    public static void main(String[] args) {
+        Set<Integer> set=new HashSet<>();
+        Scanner sc=new Scanner(System.in);
+        Linked_List ll=new Linked_List();
+        System.out.println("Enter -1 to exit :");
+        int n;
+        while(true){
+            System.out.println("Enter No :");
+            n=sc.nextInt();
+            if(n==-1){
+                break;
+            }
+            ll.insertAtLastPosition(n);
+        }
+        System.out.println("\nBefore removing duplicate elements.");
+        ll.displayLinkedList();
+        if(ll.first==null){
+            System.out.println("Empty LinkedList...");
+            return;
+        }
+        Node current=ll.first;
+        set.add(current.info);
+        while(current.link!=null){
+            current=current.link;
+            set.add(current.info);
+        }
+        ll.deleteAllNodes();
+        for (Integer element : set) {
+            ll.insertAtLastPosition(element);
+        }
+        System.out.println("\nAfter removing duplicate elements.");
+        ll.displayLinkedList();
+    }
+}
