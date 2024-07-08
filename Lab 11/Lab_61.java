46202aafe702daa15f0031042b1feece967a97e8
Nisarg Lo <140806157+NisargLo@users.noreply.github.com>
2024-07-08 09:34:41 +0530

    8-7-24

---
+++ Lab 11/Lab_61.java
@@ -0,0 +1,13 @@
+// Write a program to implement queue using singly linked list.
+
+public class Lab_61 {
+     public static void main(String[] args) {
+          
+     }
+}
+
+class Queue_Using_LinkedList{
+     Node first=null;
+
+     
+}
