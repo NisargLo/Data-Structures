ece30e0a559eda3fe34a9fc34e58ec5e10fdcfe4
Nisarg Lo <140806157+NisargLo@users.noreply.github.com>
2024-07-08 20:27:01 +0530

    Create Lab_69.java

---
+++ Lab 12/Lab_69.java
@@ -0,0 +1,44 @@
+/*
+     Write a program to remove the duplicates nodes from given sorted Linked List.
+        Input: 1 → 1 → 6 → 13 → 13 → 13 → 27 → 27
+        Output: 1 → 6 → 13 → 27
+ */
+
+import java.util.Scanner;
+
+public class Lab_69 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        Int_LinkedList ll=new Int_LinkedList();
+        System.out.println("Enter -1 to exit :");
+        int n;
+        while(true){
+            System.out.println("Enter No :");
+            n=sc.nextInt();
+            if(n==-1){
+                break;
+            }
+            ll.insertAtLastPosition(n);
+        }
+        System.out.print("\nBefore Sorting - ");
+        ll.displayLinkedList();
+        deleteDuplicates(ll.first);
+        System.out.print("\nAfter Sorting - ");
+        ll.displayLinkedList();
+    }
+
+    private static void deleteDuplicates(Node first) {
+        if (first == null || first.link == null) {
+            return;
+        }
+        Node current = first;
+        while (current.link != null) {
+            if (current.info == current.link.info) {
+                current.link = current.link.link;
+            }
+            else {
+                current = current.link;
+            }
+        }
+    }
+}
