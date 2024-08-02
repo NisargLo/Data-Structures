e4c06e1d96eb0a62afbdab42fece60fbe6b14754
Nisarg Lo <140806157+NisargLo@users.noreply.github.com>
2024-08-02 23:29:47 +0530

    2-8-24

--- Lab 08/Lab_49.java
+++ Lab 08/Lab_49.java
@@ -1,44 +1,43 @@
 /*
     Solve - https://www.codechef.com/DSAPREP_10/problems/STKSORT
+    (link is not working, so question was changed by sir)
 
-    STKSORT:
 
-    You are given an array of integers where each integer represents the height of a stack of plates.
-    You need to sort these stacks such that for any stack at index i, the stack at index i+1 has a height greater than or equal to the stack at index i.
+    Leetcode :- 155. Min Stack
 
-    To achieve this sorting, you can perform the following operations:
+    Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
 
-    1.  Merge Operation: You can merge any two adjacent stacks.
-        The cost of merging two stacks is equal to the maximum height of the two stacks being merged.
-        Your task is to find the minimum cost required to sort the array of stacks in non-decreasing order of heights using the merge operation.
+    Implement the MinStack class:
+        MinStack() initializes the stack object.
+        void push(int val) pushes the element val onto the stack.
+        void pop() removes the element on the top of the stack.
+        int top() gets the top element of the stack.
+        int getMin() retrieves the minimum element in the stack.
 
-    Input:
-        The first line contains an integer N, the number of stacks.
-        The second line contains N integers where the integer represents the height of the ith stack.
+    You must implement a solution with O(1) time complexity for each function.
 
-    Output:
-        Output a single integer representing the minimum cost required to sort the stacks.
+
+    Example 1:
+        Input
+            ["MinStack","push","push","push","getMin","pop","top","getMin"]
+            [[],[-2],[0],[-3],[],[],[],[]]
+        Output
+            [null,null,null,null,-3,null,0,-2]
+        Explanation
+            MinStack minStack = new MinStack();
+            minStack.push(-2);
+            minStack.push(0);
+            minStack.push(-3);
+            minStack.getMin(); // return -3
+            minStack.pop();
+            minStack.top();    // return 0
+            minStack.getMin(); // return -2
 
     Constraints:
-        1 ≤ N ≤ 10^5
-        1 ≤ height of each stack ≤ 10^5
-
-    Example:
-        Input:
-            5
-            3 1 4 2 5
-        Output:
-            12
-
-    Explanation:
-        To sort the stacks in non-decreasing order of heights:
-            Merge stack at index 1 (height 1) and index 2 (height 4), cost = max(1, 4) = 4
-            Merge stack at index 2 (height 4) and index 3 (height 2), cost = max(4, 2) = 4
-            Merge stack at index 3 (height 4) and index 4 (height 5), cost = max(4, 5) = 5
-        Total cost = 4 + 4 + 5 = 13
+        -231 <= val <= 231 - 1
+        Methods pop, top and getMin operations will always be called on non-empty stacks.
+        At most 3 * 104 calls will be made to push, pop, top, and getMin.
  */
-
-import java.util.*;
 
 public class Lab_49 {
     public static void main(String[] args) {
--- Lab 09/Lab_52.java
+++ Lab 09/Lab_52.java
@@ -1,4 +1,7 @@
+//
 
 public class Lab_52 {
+    public static void main(String[] args) {
 
+    }
 }
