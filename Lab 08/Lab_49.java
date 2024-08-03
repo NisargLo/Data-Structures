978c8a9af065f7bf4e9613dcf613ffa8606affd2
Nisarg Lo <140806157+NisargLo@users.noreply.github.com>
2024-08-03 22:55:36 +0530

    Update Lab_49.java

--- Lab 08/Lab_49.java
+++ Lab 08/Lab_49.java
@@ -39,6 +39,32 @@
         At most 3 * 104 calls will be made to push, pop, top, and getMin.
  */
 
-class MinStack {
+import java.util.Stack;
 
+class MinStack {
+    private final Stack<Integer> st;
+    private final Stack<Integer> minst;
+
+    public MinStack() {
+        st=new Stack<>();
+        minst=new Stack<>();
+    }
+
+    public void push(int info) {
+        st.push(info);
+        if(minst.isEmpty() || info<=minst.peek()) minst.push(info);
+    }
+
+    public void pop() {
+        int a = minst.peek();
+        if (st.pop()==a) minst.pop();
+    }
+
+    public int top() {
+        return st.peek();
+    }
+
+    public int getMin() {
+        return minst.peek();
+    }
 }
