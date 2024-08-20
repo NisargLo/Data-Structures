745ac06d437af1079d48506710317f6d0832a4c6
Nisarg Lo <140806157+NisargLo@users.noreply.github.com>
2024-08-20 17:22:40 +0530

    20-8-24

---
+++ Lab 14/Lab_77.java
@@ -0,0 +1,10 @@
+/*
+    Write a menu driven program to implement Binary Search Tree (BST) & perform following operations
+    Insert a node, Delete a node, Search a node
+ */
+
+public class Lab_77 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Lab 14/Lab_79.java
@@ -0,0 +1,20 @@
+//Write a program to check whether the given two trees are same or not.
+
+public class Lab_79 {
+    public static void main(String[] args) {
+
+    }
+
+    private static boolean isEqual(Int_Tree_Node node1, Int_Tree_Node node2){
+        if (node1==null && node2==null) {
+            return true;
+        }
+        if (node1==null || node2==null) {
+            return false;
+        }
+        if(node1.info!=node2.info){
+            return false;
+        }
+        return isEqual(node1.left,node2.left) && isEqual(node1.right,node2.right);
+    }
+}
