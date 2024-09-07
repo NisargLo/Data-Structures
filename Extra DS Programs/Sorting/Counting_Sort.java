e2466f941642f5c4ecff7e5a5c31d603e44d14bf
Nisarg Lo <140806157+NisargLo@users.noreply.github.com>
2024-09-07 20:34:39 +0530

    9-7-24

---
+++ Extra DS Programs/Sorting/Counting_Sort.java
@@ -0,0 +1,69 @@
+/*
+     Counting Sort
+
+     Time Complexity:
+     Best Case: (O(n + k))
+     Average Case: (O(n + k))
+     Worst Case: (O(n + k))
+     Description: Efficient for a small range of integers, stable.
+ */
+
+package Sorting;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Counting_Sort {
+    public static void main(String[] args) {
+          Scanner sc = new Scanner(System.in);
+          System.out.println("Enter size of array: ");
+          int n = sc.nextInt();
+          int[] a = new int[n];
+          for (int i = 0; i < a.length; i++) {
+               System.out.println("Enter element " + (i + 1) + ":");
+               a[i] = sc.nextInt();
+          }
+          sc.close();
+          System.out.println("Array before Shell Sort - " + Arrays.toString(a));
+          if(counting_Sort(a)!=null){
+               a = counting_Sort(a);
+               System.out.println("Array after Shell Sort - " + Arrays.toString(a));
+          }
+          else{
+               System.out.println("Empty Array.");
+          }
+     }
+
+     private static int[] counting_Sort(int[] a) {
+          if (a.length == 0) {
+               return null;
+          }
+
+          int max = a[0], min = a[0];
+          for (int num : a) {
+               if(num>max){
+                    max=num;
+               }
+               if(num<min){
+                    min=num;
+               }
+          }
+
+          int range=max-min+1;
+          int[] count = new int[range];
+
+          for(int i=0;i<count.length;i++){
+               count[a[i]-min]++;
+          }
+
+          for (int i=0;i<count.length-1;i++) {
+              count[i+1]+=count[i];
+          }
+
+          int[] b=new int[a.length];
+          for (int i=a.length-1;i>=0;i--) {
+              b[count[a[i]-min]-1]=a[i];
+              count[a[i]-min]--;
+          }
+          return b;
+     }
+}
