252f5af049dfd25eb465ae000dc36b3bc143a7aa
Nisarg Lo <140806157+NisargLo@users.noreply.github.com>
2024-09-19 18:40:04 +0530

    19-9-24

---
+++ Extra DS Programs/Tree/BST Iterative.java
@@ -0,0 +1,95 @@
+/*
+    Write a menu driven program to implement Binary Search Tree (BST) & perform following operations 
+    Insert a node, Delete a node, Search a node
+ */
+
+import java.util.*;
+
+public class BST {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int choice = 0;
+        int num, index;
+        while (choice != -1) {
+            System.out.println("\nEnter 1 for insertAtFirstPosition() :");
+            System.out.println("Enter 2 for insertAtLastPosition() :");
+            System.out.println("Enter 3 for insertAtOrder() :");
+            System.out.println("Enter 4 for insertAtSpecificPosition() :");
+            System.out.println("Enter -1 to exit :");
+            choice = sc.nextInt();
+            switch (choice) {
+                case 1 -> {
+                    System.out.println("Enter Number :");
+                    num = sc.nextInt();
+                }
+
+                case 2 -> {
+                    System.out.println("Enter Number :");
+                    num = sc.nextInt();
+                }
+
+                case 3 -> {
+                    System.out.println("Enter Number :");
+                    num = sc.nextInt();
+                }
+
+                case 4 -> {
+                    System.out.println("Enter Index :");
+                    index = sc.nextInt();
+                    System.out.println("Enter Number :");
+                    num = sc.nextInt();
+                }
+
+                case -1 -> System.out.println("Program Over...");
+
+                default -> System.out.println("Invalid");
+            }
+        }
+
+    }
+}
+
+class TreeNode {
+    TreeNode left;
+    TreeNode right;
+    int data;
+
+    TreeNode(int data) {
+        this.data = data;
+    }
+}
+
+class BinarySearchTree {
+    TreeNode root;
+
+    protected void insertIterative(int data) {
+        TreeNode newNode = new TreeNode(data);
+        if (root == null) {
+            root = newNode;
+            return;
+        }
+        TreeNode current = root;
+        TreeNode parent = null;
+        while (true) {
+            parent = current;
+            if (data < current.data) {
+                current = current.left;
+                if (current == null) {
+                    parent.left = newNode;
+                    return;
+                }
+            } else {
+                current = current.right;
+                if (current == null) {
+                    parent.right = newNode;
+                    return;
+                }
+            }
+        }
+    }
+
+    
+    protected void display(int data) {
+
+    }
+}
--- Lab 22/Lab_98.java
+++ Lab 22/Lab_98.java
@@ -15,7 +15,7 @@ public class Lab_98 {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter size of array: ");
 		int n = sc.nextInt();
-		if(n==0){
+		if (n == 0) {
 			System.out.println("Empty Array...");
 			return;
 		}
@@ -25,12 +25,44 @@ public class Lab_98 {
 			a[i] = sc.nextInt();
 		}
 		sc.close();
-		System.out.println("Array before Shell Sort - " + Arrays.toString(a));
-		heap_Sort();
-		System.out.println("Array after Shell Sort - " + Arrays.toString(a));
+		System.out.println("Array before Heap Sort - " + Arrays.toString(a));
+		heap_Sort(a);
+		System.out.println("Array after Heap Sort - " + Arrays.toString(a));
 	}
-	
-	private static void heap_Sort(){
-	
+
+	private static void heap_Sort(int[] a) {
+		int l = a.length;
+
+		// l/2-1 -> gives index of last parent
+		for (int i = l / 2 - 1; i >= 0; i--) {
+			heapify(a, l, i);
+		}
+
+		for (int i = l - 1; i >= 0; i--) {
+			int temp = a[i];
+			a[i] = a[0];
+			a[0] = temp;
+			heapify(a, i, 0);
+		}
+	}
+
+	private static void heapify(int[] a, int l, int root) {
+		int max = root;
+		int left = 2 * root + 1;
+		int right = 2 * root + 2;
+
+		if (left < l && a[left] > a[max]) {
+			max = left;
+		}
+		if (right < l && a[right] > a[max]) {
+			max = right;
+		}
+
+		if (max != root) {
+			int temp = a[root];
+			a[root] = a[max];
+			a[max] = temp;
+			heapify(a, l, max);
+		}
 	}
 }
